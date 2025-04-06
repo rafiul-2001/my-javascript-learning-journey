7d3d02ffb206c1760a6e9801169ea74e832657e6
rafiul-2001 <rafiulislam2001@gmail.com>
2025-04-06 09:43:51 +0600

    usage of template string

---
+++ showingOutputTemplateStringMethod.js
@@ -0,0 +1,13 @@
+//uses back tick (``) 
+// for the variable ${variable}
+
+let name= "Rafiul";
+console.log(`My name is ${name}`);
+
+// how do u will print in new line??
+
+console.log(`My name is
+${name}`);  // this is not funny
+
+//I will use this instead
+console.log(`My name is\n${name}`);
