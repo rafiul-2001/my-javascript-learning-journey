41f05f57f090fcfaf40901fcb39edf664da10dd6
rafiul-2001 <rafiulislam2001@gmail.com>
2025-04-07 17:50:30 +0600

    #

---
+++ importantInfo.js
@@ -0,0 +1,5 @@
+// variable changed it data type !!! what the heck
+let a="abc";
+console.log(typeof(a));
+a=56;
+console.log(typeof(a));
---
+++ stringTrueFalseTricks.js
@@ -0,0 +1,12 @@
+let a = '';
+
+
+if(a){
+    console.log("first");
+}
+
+a='any string or none zero';
+
+if(a){
+    console.log("Second");
+}
