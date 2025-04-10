e9ca133049c031d31e26fcb70e2ba77a5dd61781
rafiul-2001 <rafiulislam2001@gmail.com>
2025-04-10 15:41:41 +0600

    #

---
+++ array.js
@@ -0,0 +1,27 @@
+/*let name = ["ab", "bc", "cd"];
+console.log(name); // it will display as [ 'ab', 'bc', 'cd' ]
+
+name.push("de");
+console.log(name);  // it will be: [ 'ab', 'bc', 'cd', 'de' ]
+name.pop();
+console.log(name.length());
+*/
+
+
+
+
+
+//size declaration
+let arr = new Array(4);
+console.log(arr[3]); // not initilize thus : undefine
+
+
+
+
+
+
+// concatenate array
+let a = ["ab", "bc", "cd"];
+let b = ["ab", "bc", "cd"];
+let c = a.concat(b);
+console.log(c);
