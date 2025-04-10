3a9e0b85ac0bf63a047aa5c32f5dcfe3993f76f6
rafiul-2001 <rafiulislam2001@gmail.com>
2025-04-10 18:13:20 +0600

    some tricks and tips

--- importantInfo.js
+++ importantInfo.js
@@ -3,3 +3,15 @@ let a="abc";
 console.log(typeof(a));
 a=56;
 console.log(typeof(a));
+
+
+
+// sort function works on string not on numbers. to sort number u have to add anonymous function
+let ar=[9,8,7,6,5,4,65,8,4,9,1,678,1];
+ar.sort();
+console.log(ar); // doesn't work
+
+ar.sort(function(a,b){
+    return a-b;
+});
+console.log(ar); // now it is working
