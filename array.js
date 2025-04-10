26a7a0c23a7bd673d7140951d45679653ff75f13
rafiul-2001 <rafiulislam2001@gmail.com>
2025-04-10 18:07:43 +0600

    array functionality

--- array.js
+++ array.js
@@ -1,3 +1,9 @@
+// pop, opposite of push in js is shift. shift remove element from the begining
+// push,opposite unshift. unshift add element in the begining of the array exe:arr.unshift(4)   
+
+
+
+
 /*let name = ["ab", "bc", "cd"];
 console.log(name); // it will display as [ 'ab', 'bc', 'cd' ]
 
@@ -25,3 +31,15 @@ let a = ["ab", "bc", "cd"];
 let b = ["ab", "bc", "cd"];
 let c = a.concat(b);
 console.log(c);
+
+
+
+
+
+// confusion: 
+let abc = new Array(1);
+console.log(abc.length);
+abc.push(3);
+abc.push(4);
+abc.push(5);
+console.log(abc.length);
